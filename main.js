59b1abbfbec9fd57771628f255fb05cd1282e71b
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-04-05 19:52:37 +0300

    feat: add project files

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ let items = [];
 
 const name = document.getElementById("itemName");    
 const price = document.getElementById("itemPrice");
-
-function addItem(name, price) {
-
-}
+const addButton = document.querySelector("addButton");
+const displayList = document.getElementById("displayList");
 
 function checkInputType(name, price) {
     if (typeof price !== number && typeof name !== string ) {
